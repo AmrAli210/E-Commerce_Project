380218cabbc8b31bf9e4b8671d50c3a593f57970
Amr Ali <amr40267@gmail.com>
2023-10-06 19:19:13 +0300

    inprogress

--- src/app/modules/products/components/product-info/product-info.component.ts
+++ src/app/modules/products/components/product-info/product-info.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import { Product } from 'src/app/shared/interfaces/product.model';
 
 @Component({
   selector: 'app-product-info',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-info.component.scss']
 })
 export class ProductInfoComponent {
+  quantity: number = 1;
 
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Product) {}
+
+  addItem() {
+    if (this.data.quantity > this.quantity) {
+      this.quantity += 1;
+    }
+    
+  }
+
+  removeItem() {
+    if (this.quantity > 1) {
+      this.quantity -= 1;
+    }
+  }
 }
